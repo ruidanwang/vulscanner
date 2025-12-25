2837f75f751abab97e50895c81032b37738edf11
ruidanwang <newbiner@gmail.com>
2025-12-25 09:22:25 +0000

    update

--- pkg/handler/vulscan/vulscan.go
+++ pkg/handler/vulscan/vulscan.go
@@ -1,32 +1,46 @@
 package vulscan
 
 import (
-	"bytes"
-	"fmt"
-	"os/exec"
+	"context"
+
+	nuclei "github.com/projectdiscovery/nuclei/v3/lib"
 
 	"github.com/gin-gonic/gin"
 )
 
 func VulScan(c *gin.Context) {
-	target := "https://example.com"
-	template := "/opt/nuclei-templates/cves/2023/CVE-2023-12345.yaml"
-
-	cmd := exec.Command(
-		"nuclei",
-		"-u", target,
-		"-t", template,
-		"-json",
-		"-silent",
+	ne, err := nuclei.NewNucleiEngineCtx(context.Background(),
+		nuclei.WithTemplateFilters(nuclei.TemplateFilters{Tags: []string{"oast"}}),
+		nuclei.EnableStatsWithOpts(nuclei.StatsOptions{MetricServerPort: 6064}), // optionally enable metrics server for better observability
 	)
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("scan error:", err)
-		return
+	if err != nil {
+		panic(err)
 	}
+	// load targets and optionally probe non http/https targets
+	ne.LoadTargets([]string{"http://honey.scanme.sh"}, false)
+	err = ne.ExecuteWithCallback(nil)
+	if err != nil {
+		panic(err)
+	}
+	defer ne.Close()
+	// target := "https://example.com"
+	// template := "/opt/nuclei-templates/cves/2023/CVE-2023-12345.yaml"
 
-	fmt.Println(out.String())
+	// cmd := exec.Command(
+	// 	"nuclei",
+	// 	"-u", target,
+	// 	"-t", template,
+	// 	"-json",
+	// 	"-silent",
+	// )
+
+	// var out bytes.Buffer
+	// cmd.Stdout = &out
+
+	// if err := cmd.Run(); err != nil {
+	// 	fmt.Println("scan error:", err)
+	// 	return
+	// }
+
+	// fmt.Println(out.String())
 }
